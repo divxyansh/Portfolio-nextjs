208cde47716cce4ad4c80c7ffcd73f23652206c8
Divi <mac@Macs-MacBook-Air-2.local>
2024-09-11 02:37:52 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Hero from "../components/main/Hero";
+import Skills from "../components/main/Skills";
+import Encryption from "../components/main/Encryption";
+import Projects from "../components/main/Projects";
+
+export default function Home() {
+  return (
+    <main className="h-full w-full">
+      <div className="flex flex-col gap-20">
+        <Hero />
+        <Skills/>
+        <Projects/>
+        <Encryption/>
+      </div>
+    </main>
+  );
+}
